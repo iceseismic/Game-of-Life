e7aa5591c739d30c54049cccbf60849fd613f008
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-05-10 10:49:05 +1200

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,20 @@
+/**
+ * @author (Joel Bannister)
+ * @version (10.5.22)
+ */
+import java.util.Scanner;
+public class Game
+{
+    //reading input from the user
+    Scanner kb = new Scanner(System.in);
+    
+
+    
+    public Game()
+    {
+     
+    }
+
+    
+    
+}
