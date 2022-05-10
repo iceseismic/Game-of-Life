a29f91ba131857d45e5e2495a1adf79b26004edb
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-05-10 17:23:32 +1200

    added basic introduction + mode select

--- Game.java
+++ Game.java
@@ -2,19 +2,50 @@
  * @author (Joel Bannister)
  * @version (10.5.22)
  */
-import java.util.Scanner;
+import java.util.Scanner; //get rid of slashes  at bottom when connected this file to online file
 public class Game
 {
     //reading input from the user
-    Scanner kb = new Scanner(System.in);
-    
+    Scanner keyin = new Scanner(System.in);
+    String modeChoice;
+    boolean gameModePick=false;
+    boolean offline;
 
-    
+    //variables begin/set up gamne
+
     public Game()
     {
-     
-    }
+        System.out.println("Heeeelloo!.. today, I introduce to you Joel's Game of Life! /n"
+            + "This is the run down:\n"
+            + "\n");
 
-    
-    
+            
+
+        //Game's rules explanation
+        System.out.println("Now, what do you feel like playing? \n"
+            +"Singleplayer is by yourself, and online is against a friend \n"
+            +"type 'Single' for Singleplayer, or 'Multi' for Multiplayer");
+
+        while(!gameModePick){
+            modeChoice=keyin.nextLine().toLowerCase();
+            if(modeChoice.equals("multi")){
+                gameModePick = true;
+                offline=false;
+            }else if (modeChoice.equals("single")){
+                gameModePick = false;
+                offline=true;
+            }
+            else {
+                System.out.println("Please choose either mode");
+            }
+
+            if(offline){
+                  //new Singleplayer"();
+            }else{  
+                  //new online"();
+            }
+            
+        }                       
+    }                                
 }
+
